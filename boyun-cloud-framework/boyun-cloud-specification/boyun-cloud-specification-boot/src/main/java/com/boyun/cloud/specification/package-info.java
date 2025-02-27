cebc3f6cbe364fa17256d4b33649982d1fb25441
marklin <2273990296@qq.com>
2025-02-27 18:10:22 +0800

    commit to project

---
+++ boyun-cloud-framework/boyun-cloud-specification/boyun-cloud-specification-boot/src/main/java/com/boyun/cloud/specification/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @className: com.boyun.cloud.specification.package-info
+ * @projectName: 项目
+ * @module: 项目-package-info类，主要位于模块
+ * @content:
+ * @author: Powered by Marklin
+ * @datetime: 2025-02-27 18:09
+ * @version:
+ * @copyright: Copyright © 2018-2025  Systems Incorporated. All rights reserved.
+ */
+package com.boyun.cloud.specification;
