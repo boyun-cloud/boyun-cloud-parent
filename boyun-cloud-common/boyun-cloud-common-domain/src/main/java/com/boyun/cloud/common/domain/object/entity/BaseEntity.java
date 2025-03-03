1b4ec506978286992b64868106529ed8a137538f
marklin <2273990296@qq.com>
2025-03-03 13:31:10 +0800

    commit to project

--- boyun-cloud-common/boyun-cloud-common-domain/src/main/java/com/boyun/cloud/common/domain/object/entity/BaseEntity.java
+++ boyun-cloud-common/boyun-cloud-common-domain/src/main/java/com/boyun/cloud/common/domain/object/entity/BaseEntity.java
@@ -1,6 +1,11 @@
 package com.boyun.cloud.common.domain.object.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.boyun.cloud.common.domain.Entity;
+import lombok.*;
+
+import java.util.Date;
 
 /**
  * @className: com.boyun.cloud.common.domain.object.entity.BaseEntity
@@ -12,8 +17,76 @@ import com.boyun.cloud.common.domain.Entity;
  * @version: 1.0.0
  * @copyright: Copyright © 2018-2025 BoyunCloud Systems Incorporated. All rights reserved.
  */
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode()
 public class BaseEntity implements Entity {
 
     private static final long serialVersionUID = 1363028260282722381L;
+    /**
+     * 版本号
+     */
+    @Getter
+    @Setter
+    @TableField(value = Entity.VERSION)
+    private Integer version;
 
+    /**
+     * 创建人
+     */
+    @Getter
+    @Setter
+    @TableField(value = Entity.CREATOR_ID, fill = FieldFill.INSERT)
+    private Long creatorId;
+
+    /**
+     * 创建时间
+     */
+    @Getter
+    @Setter
+    @TableField(value = Entity.CREATED_TIME, fill = FieldFill.INSERT)
+    private Date createdTime;
+
+
+    /**
+     * 更新人
+     */
+    @Getter
+    @Setter
+    @TableField(value = Entity.UPDATOR_ID, fill = FieldFill.INSERT_UPDATE)
+    private Long updatorId;
+
+
+    /**
+     * 更新时间
+     */
+    @Getter
+    @Setter
+    @TableField(value = Entity.UPDATED_TIME, fill = FieldFill.INSERT_UPDATE)
+    private Date updatedTime;
+
+    /**
+     * 逻辑删除
+     */
+    @Getter
+    @Setter
+    @TableField(value = Entity.DELETED)
+    private Integer deleted;
+
+    /**
+     * 版本号
+     */
+    @Getter
+    @Setter
+    @TableField(value = Entity.STATUS)
+    private Integer status;
+
+    /**
+     * 备注
+     */
+    @Getter
+    @Setter
+    @TableField(value = Entity.REMARK)
+    private String remark;
 }
