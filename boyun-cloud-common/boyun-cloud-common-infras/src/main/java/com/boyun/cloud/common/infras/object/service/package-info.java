a61612844a469dc8051293af92ea570fe9cdcb54
Marklin <marklindemo@163.com>
2025-04-11 23:12:25 +0800

    commit to boyun-cloud-dependencies

---
+++ boyun-cloud-common/boyun-cloud-common-infras/src/main/java/com/boyun/cloud/common/infras/object/service/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @className: com.boyun.cloud.common.infras.object.service.package-info
+ * @projectName: 项目
+ * @module: 项目-package-info类，主要位于模块
+ * @content:
+ * @author: Powered by Marklin
+ * @datetime: 2025-04-11 22:33
+ * @version:
+ * @copyright: Copyright © 2018-2025  Systems Incorporated. All rights reserved.
+ */
+package com.boyun.cloud.common.infras.object.service;
